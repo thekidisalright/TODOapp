1be003de4ed8277d588a522ea00bac5a15a23f67
thekidisalright <dev.alexandresilva@gmail.com>
2026-04-20 00:34:26 -0300

    fix: api url not working

--- FRONTEND/TODOapp/src/app/app.ts
+++ FRONTEND/TODOapp/src/app/app.ts
@@ -13,7 +13,7 @@ export class App {
   arrayDeTarefas = signal<Tarefa[]>([]);
   apiURL: string;
   constructor(private http: HttpClient) {
-    this.apiURL = 'https://apitodoapp-ppxk.onrender.com/';
+    this.apiURL = 'https://apitodoapp-ppxk.onrender.com';
     this.READ_tarefas();
   }
   CREATE_tarefa(descricaoNovaTarefa: string) {
